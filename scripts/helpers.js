ef3d6e89730bb2cd4560644e50847e873daec1ff
Melih Karav <melihkarav26@gmail.com>
2021-02-20 19:00:23 +0000

    Filtreleme genre ve yeara göre yapıldı, submit clear yapıldı

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -5,3 +5,17 @@ export const searchMovieByTitle = (movie, searchValue) => {
 export const makeBgActive = (movie) => {
     document.querySelector(`tr[data-id='${movie.id}']`).style.background = "#d7f0f7";
 }
+
+export const createButton = (id, type, classes, text) => {
+    const $btn = document.createElement('button');
+    
+    $btn.setAttribute('id', id);
+    $btn.setAttribute('type', type);
+
+    for (let i = 0; i < classes.length; ++i) {
+      $btn.classList.add(classes[i]);
+    }
+    $btn.innerText = text;
+    return $btn;
+  };
+  
